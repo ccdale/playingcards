7c746ad5d9816a05d2c12a8561d1eda6c728216f
Chris Allison <chris.charles.allison@gmail.com>
2017-09-24 07:31:26 +0100

    using distutils.core for setup
    and implementing python-pre-commit.sh hook

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from distutils.core import setup
 
-config = {
-    'description': 'Set of classes to represent playing cards in python games',
-    'author': 'Chris Allison',
-    'url': 'https://github.com/ccdale/playingcards',
-    'download_url': 'https://github.com/ccdale/playingcards',
-    'author_email': 'chris.charles.allison+playingcards@gmail.com',
-    'version': '1.0.3',
-    'install_requires': [],
-    'packages': ['playingcards'],
-    'package_dir': {'playingcards':''},
-    'package_data': {'':'img/*.png'},
-    'scripts': [],
-    'name': 'playingcards'
-}
-setup(**config)
+setup(
+    description='Set of classes to represent playing cards in python games',
+    author='Chris Allison',
+    url='https://github.com/ccdale/playingcards',
+    download_url='https://github.com/ccdale/playingcards',
+    author_email='chris.charles.allison+playingcards@gmail.com',
+    version='1.0.5',
+    install_requires=[],
+    packages=['playingcards'],
+    package_dir={'playingcards':''},
+    package_data={'':'img/*.png'},
+    scripts=[],
+    name='playingcards'
+)
