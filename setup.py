82287e34c2d4bc18c66e7849c3f6339822ececef
Chris Allison <chris.charles.allison@gmail.com>
2017-09-25 10:26:55 +0100

    added license and classification information

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from distutils.core import setup
 
 setup(
+    name='playingcards',
     description='Set of classes to represent playing cards in python games',
     author='Chris Allison',
     url='https://github.com/ccdale/playingcards',
     download_url='https://github.com/ccdale/playingcards',
     author_email='chris.charles.allison+playingcards@gmail.com',
-    version='1.0.19',
+    version='1.0.21',
+    license='MIT',
     packages=[''],
     scripts=[],
-    name='playingcards'
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Topic :: Games/Entertainment',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.5',
+      ],
 )
